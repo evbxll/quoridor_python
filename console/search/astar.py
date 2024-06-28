c10d1be51a78a46add55e5a3888f30700d0b7eaf
Evan Bell (Deskt WSL) <embell@mit.edu>
2024-06-28 18:55:14 -0400

    made astar impl

--- console/search/astar.py
+++ console/search/astar.py
@@ -1,38 +1,75 @@
-from collections import deque
-
-
-dirs = [(1,0), (0,1), (0,-1), (-1,0)]
-
-def bfs_shortest_to_exit(game_state, path_none):
-    p1 = tuple(game_state.player1_pos)
-    p2 = tuple(game_state.player2_pos)
+import numpy as np
+import heapq
 
 
 
-    def bfs_dist(game_state, start, end_row, other_player_pos, skip_other_player_parity = 0):
+class Node:
+    def __init__(self, x, y, f_score):
+        self.x = x
+        self.y = y
+        self.f_score = f_score
 
-        if start[0] == end_row:
-            return path_none
-        visited = {start,}
-        queue = deque([(start, 0)])  # (node, path_length)
+    def __lt__(self, other):
+        return self.f_score < other.f_score
 
-        possible_skip_other_len = float('inf')
+def z_asearch(game_state, start_pos : tuple, end_row : int, other_player : tuple):
 
-        while queue:
-            pos, path_length = queue.popleft()
-            for x,y in [ (pos[0]+i, pos[1] + j) for i,j in dirs]:
-                # if in bounds and NO wall between player and new_pos
-                if 0 <= x < game_state.size and 0 <= y < game_state.size and not game_state.is_wall_blocking_path(pos, (x,y)):
-                    if x == end_row:
-                        return min(path_length + 1, possible_skip_other_len)
-                    elif (x,y) not in visited:
-                        visited.add((x,y))
-                        if other_player_pos == (x,y) and path_length%2 == skip_other_player_parity:
-                            possible_skip_other_len = path_length + bfs_dist(game_state, other_player_pos, end_row, (-1,-1))
-                        queue.append(((x,y), path_length + 1))
+    base_val = -1
+    start_val = 0
 
-        return float('inf')
+    if start_pos[0] == end_row:
+        return base_val
 
-    dist1 = bfs_dist(game_state, p1, 0, p2, 1*(not game_state.player1)) - 1*(game_state.player1)
-    dist2 = bfs_dist(game_state, p2, game_state.size - 1, p1, 1*(game_state.player1)) - 1*(not game_state.player1)
-    return (dist1, dist2)
+    size = game_state.size
+
+    flip_dirs = 1 if end_row == 0 else -1
+    directions = flip_dirs * np.array([[-1, 0], [0, 1], [1, 0], [0, -1]])  # Up, Down, Left, Right
+
+    visited = np.zeros((size, size), dtype=bool)
+    p_queue = []
+    heapq.heappush(p_queue, Node(start_pos[0], start_pos[1], 0))
+
+    g_score = np.full((size, size), np.inf)
+    g_score[start_pos[0], start_pos[1]] = start_val
+
+    while p_queue:
+        current = heapq.heappop(p_queue)
+        x, y = current.x, current.y
+
+        if x == end_row:
+            return current.f_score
+
+        if not visited[x, y]:
+            visited[x, y] = True
+
+            for dx, dy in directions:
+                new_x = x + dx
+                new_y = y + dy
+
+                if 0 <= new_x < size and 0 <= new_y < size and game_state.is_valid_move((x, y), (new_x, new_y)):
+                    tentative_g_score = g_score[x, y] + 1
+
+
+                    ## check if have parity to jump, and its our move
+                    if (tentative_g_score - start_val) < 3 and (new_x, new_y) == other_player and new_x != end_row and (tentative_g_score - start_val) % 2 == 1:
+                        if (game_state.player1 and end_row == 0): # player1 to move, we are player 1
+                            tentative_g_score = g_score[x, y]
+                        elif (not game_state.player1 and end_row != 0): #player2 to move, we are player 2
+                            tentative_g_score = g_score[x, y]
+
+                    if tentative_g_score < g_score[new_x, new_y]:
+                        g_score[new_x, new_y] = tentative_g_score
+                        f_score = tentative_g_score + abs(new_x - end_row)
+                        heapq.heappush(p_queue, Node(new_x, new_y, f_score))
+
+    return np.inf
+
+def astar_search(game_state):
+    '''
+    Calculates path distance from start_pos to end_row, accounting for other_player and whether a jump can happen using skip_parity
+
+    game_state is a GameState type
+    '''
+    p1 = z_asearch(game_state, game_state.player1_pos, 0, game_state.player2_pos)
+    p2 = z_asearch(game_state, game_state.player2_pos, game_state.size - 1, game_state.player1_pos)
+    return p1, p2
